cb80ba0531b077800f5fe55cb6f2d9ff4aefbcf3
ariaslir <ariaslir@unlv.nevada.edu>
2026-09-10 04:02:50 +0000

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include <kernel/types.h>
+#include <kernel/stat.h>
+#include <user/user.h>
+
+int main(){
+    printf("Hello World ! n");
+    printf("Name: Jesse Arias");
+    printf("Section: 1001");
+    exit(0);
+}
