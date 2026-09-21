e28db52d96d21ed17df90c13caae474ff2667ef2
Jesse Arias Lirio <ariaslir@unlv.nevada.edu>
2026-09-21 15:59:45 -0700

    added exit in child

--- user/testtracing.c
+++ user/testtracing.c
@@ -5,7 +5,7 @@ int main(){
     int pid = getpid();
     printf("Process ID: %d\n", pid);
     
-    uptime();
+    //uptime();
     printf("Process Uptime: %d\n", uptime());
     printf("Testing syscalltrace() Implementation...");
     syscalltrace(1);
@@ -13,10 +13,11 @@ int main(){
     int checkForkVal = fork();
     
     if(checkForkVal > 0){
-        printf("Parent Process ID: %d\n", getpid());
+        printf("Parent Process ID: %d\n", pid);
     }
     else{
         printf("Child Process ID: %d\n", getpid());
+        exit(0);
     }
 
     return 0;
