a0ed87deb5e5e761e52fffb895b811093301da80
Jesse Arias Lirio <ariaslir@unlv.nevada.edu>
2026-09-22 10:25:29 -0700

    Created an array to match to the string name of syscalls and updated if statement to print actuall names

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -134,8 +134,34 @@ static uint64 (*syscalls[])(void) = {
   [SYS_syscalltrace] = sys_syscalltrace, // added syscalltrace to array of syscalls
   // clang-format on
 };
-
-
+// added in this array to store names and output the names
+static const char *syscall_names[] =  {
+  // clang-format off 
+  [SYS_fork]    = "fork",
+  [SYS_exit]    = "exit",
+  [SYS_wait]    = "wait",
+  [SYS_pipe]    = "pipe",
+  [SYS_read]    = "read",
+  [SYS_kill]    = "kill",
+  [SYS_exec]    = "exec",
+  [SYS_fstat]   = "fstat",
+  [SYS_chdir]   = "chdir",
+  [SYS_dup]     = "dup",
+  [SYS_getpid]  = "getpid",
+  [SYS_sbrk]    = "sbrk",
+  [SYS_pause]   = "pause",
+  [SYS_uptime]  = "uptime",
+  [SYS_open]    = "open",
+  [SYS_write]   = "write",
+  [SYS_mknod]   = "mknod",
+  [SYS_unlink]  = "unlink",
+  [SYS_link]    = "link",
+  [SYS_mkdir]   = "mkdir",
+  [SYS_close]   = "close",
+  [SYS_sync]    = "sync",
+  [SYS_syscalltrace] = "syscalltrace", // added syscalltrace to array of syscalls
+  // clang-format on
+};
 
 void
 syscall(void)
@@ -148,7 +174,9 @@ syscall(void)
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
     if(p->traceValue > 0){
-      printk("System Call: %s\n", myproc()->name);
+      // need to see how to get actual names, probably make a whole new array of strings
+      // that has the names stored
+      printk("System Call: %s\n", syscall_names[num]);
       printk("Process ID: %d\n", myproc()->pid);
     }
     p->trapframe->a0 = syscalls[num]();
