27a0d491b26bffd9a68fa824c068ee110487326c
Jesse Arias Lirio <ariaslir@unlv.nevada.edu>
2026-09-22 10:33:56 -0700

    Modified if statement to temporarily set trace value back to 0 to attempt to fix printing errors

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -176,8 +176,11 @@ syscall(void)
     if(p->traceValue > 0){
       // need to see how to get actual names, probably make a whole new array of strings
       // that has the names stored
+      int temp = traceValue;
+      p->traceValue = 0;
       printk("System Call: %s\n", syscall_names[num]);
       printk("Process ID: %d\n", myproc()->pid);
+      p->traceValue = temp;
     }
     p->trapframe->a0 = syscalls[num]();
   } else {
