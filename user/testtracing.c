22b4ebce54f7b74c4f3f14a11407247dae2b1eda
Jesse Arias Lirio <ariaslir@unlv.nevada.edu>
2026-09-22 21:43:55 -0700

    Trying to add printing into testtracing

--- user/testtracing.c
+++ user/testtracing.c
@@ -1,8 +1,11 @@
 #include "user.h"
+#include "proc.h"
 
 int main(){
 
     //int pid = getpid();
+    struct proc *np;
+    struct proc *p = myproc();
     
     //uptime();
     printf("Process Uptime: %d\n", uptime());
@@ -13,6 +16,12 @@ int main(){
     getpid();
     pause(50);
     fork();
+    if(p->traceValue > 0){
+        printf("Parent Tracking Value: %d\n", p->traceValue);
+    }
+    else{
+        printf("Child Tracking Value: %d\n", p->traceValue);
+    }
     uptime();//added uptime to match output
 
     return 0;
